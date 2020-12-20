730602cbf28241456d879a6d981b0e60f26500eb
Restyled.io <commits@restyled.io>
2020-12-20 13:25:17 +0000

    Restyled by clang-format

--- Assets/Scripts/Platform Scripts/PlatfromScript.cs
+++ Assets/Scripts/Platform Scripts/PlatfromScript.cs
@@ -3,100 +3,81 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatfromScript : MonoBehaviour {
-    public float move_Speed = 2f;
-    public float bound_Y = 6f;
+  public float move_Speed = 2f;
+  public float bound_Y = 6f;
 
-    public bool is_Moving_Platfrom_Left,
-           is_Moving_Platfrom_Right,
-           is_Breakable_Platfrom,
-           is_Spike_Platfrom,
-           is_Platfrom;
+  public bool is_Moving_Platfrom_Left, is_Moving_Platfrom_Right,
+      is_Breakable_Platfrom, is_Spike_Platfrom, is_Platfrom;
 
-    private Animator anim;
+  private Animator anim;
 
-    void Awake () {
-        if (is_Breakable_Platfrom == true) {
-            anim = GetComponent<Animator> ();
-        }
+  void Awake() {
+    if (is_Breakable_Platfrom == true) {
+      anim = GetComponent<Animator>();
     }
+  }
 
-    // Update is called once per frame
-    void Update () {
-        Move ();
+  // Update is called once per frame
+  void Update() { Move(); }
+  // moving every single platform is not a good idea. 100 platforms - 100 calls
+  // to update - performance not efficient. better having a single object that is
+  // moving and it has platform objs as children
+  void Move() {
+    Vector2 temp = transform.position;
+    temp.y += move_Speed * Time.deltaTime;
+    transform.position = temp;
+
+    if (temp.y >= bound_Y) {
+      gameObject.SetActive(false);
     }
-    //moving every single platform is not a good idea. 100 platforms - 100 calls to update - performance not efficient. better having a single object that is moving and it has platform objs as children
-    void Move () {
-        Vector2 temp = transform.position;
-        temp.y += move_Speed * Time.deltaTime;
-        transform.position = temp;
+  } // move
 
-        if (temp.y >= bound_Y) {
-            gameObject.SetActive (false);
-        }
-    } // move
+  void BreakableDeactivate() {
+    Invoke("DeactivateGameObject", 0.3f); // magic strings is bad idea, if you
+                                          // rename method - will not work
+  }
 
+  void DeactivateGameObject() {
+    SoundManager.instance
+        .IceBreakSound(); // singletons used in that way is also not good for
+                          // architecture - makes your components strongly
+                          // dependant from each other
+    gameObject.SetActive(false);
+  }
 
-    void BreakableDeactivate()
-    {
-        Invoke("DeactivateGameObject", 0.3f); //magic strings is bad idea, if you rename method - will not work
+  void OnTriggerEnter2D(Collider2D target) {
+    if (target.tag == "Player") {
+      if (is_Spike_Platfrom) {
+        target.transform.position = new Vector2(1000f, 1000f);
+        SoundManager.instance.GameOverSound();
+        GameManager.instance.RestartGame();
+      }
     }
+  } //
 
-
-    void DeactivateGameObject()
-    {
-        SoundManager.instance.IceBreakSound();//singletons used in that way is also not good for architecture - makes your components strongly dependant from each other
-        gameObject.SetActive(false);
+  void OnCollisionEnter2D(Collision2D target) {
+    if (target.gameObject.tag == "Player") {
+      if (is_Breakable_Platfrom) {
+        SoundManager.instance.LandSound();
+        anim.Play("Break");
+      }
+      if (is_Platfrom) {
+        SoundManager.instance.LandSound();
+      }
     }
+  } //
 
-
-    void OnTriggerEnter2D(Collider2D target)
-    {
-        if (target.tag == "Player")
-        {
-            if (is_Spike_Platfrom)
-            {
-                target.transform.position = new Vector2(1000f, 1000f);
-                SoundManager.instance.GameOverSound();
-                GameManager.instance.RestartGame();
-            }
-        }
-    } //
-
-
-    void OnCollisionEnter2D(Collision2D target)
-    {
-        if (target.gameObject.tag == "Player")
-        {
-            if (is_Breakable_Platfrom)
-            {
-                SoundManager.instance.LandSound();
-                anim.Play("Break");
-
-            }
-            if (is_Platfrom)
-            {
-                SoundManager.instance.LandSound();
-
-            }
-        }
-    }//
-
-    void OnCollisionStay2D(Collision2D target)
-    {
-        if (target.gameObject.tag == "Player")
-        {
-            if (is_Moving_Platfrom_Left)
-            {
-                target.gameObject.GetComponent<PlayerMovement>().PlatformMove(-1f);
-            }
-            if (is_Moving_Platfrom_Right)
-            {
-                target.gameObject.GetComponent<PlayerMovement>().PlatformMove(1f);
-            }
-        }
+  void OnCollisionStay2D(Collision2D target) {
+    if (target.gameObject.tag == "Player") {
+      if (is_Moving_Platfrom_Left) {
+        target.gameObject.GetComponent<PlayerMovement>().PlatformMove(-1f);
+      }
+      if (is_Moving_Platfrom_Right) {
+        target.gameObject.GetComponent<PlayerMovement>().PlatformMove(1f);
+      }
     }
+  }
 
-//many responsibilities in a single script
+  // many responsibilities in a single script
 
-
-}//class
+} // class
