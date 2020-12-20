cbbbf62902b95611967fcd883a782659a66ee32e
Mikhail Shpirko <mikhail.shpirko@gmail.com>
2020-12-20 18:25:00 +0500

    code review

--- Assets/Scripts/Platform Scripts/PlatfromScript.cs
+++ Assets/Scripts/Platform Scripts/PlatfromScript.cs
@@ -24,7 +24,7 @@ public class PlatfromScript : MonoBehaviour {
     void Update () {
         Move ();
     }
-
+    //moving every single platform is not a good idea. 100 platforms - 100 calls to update - performance not efficient. better having a single object that is moving and it has platform objs as children
     void Move () {
         Vector2 temp = transform.position;
         temp.y += move_Speed * Time.deltaTime;
@@ -38,13 +38,13 @@ public class PlatfromScript : MonoBehaviour {
 
     void BreakableDeactivate()
     {
-        Invoke("DeactivateGameObject", 0.3f);
+        Invoke("DeactivateGameObject", 0.3f); //magic strings is bad idea, if you rename method - will not work
     }
 
 
     void DeactivateGameObject()
     {
-        SoundManager.instance.IceBreakSound();
+        SoundManager.instance.IceBreakSound();//singletons used in that way is also not good for architecture - makes your components strongly dependant from each other
         gameObject.SetActive(false);
     }
 
@@ -96,7 +96,7 @@ public class PlatfromScript : MonoBehaviour {
         }
     }
 
-
+//many responsibilities in a single script
 
 
 }//class
