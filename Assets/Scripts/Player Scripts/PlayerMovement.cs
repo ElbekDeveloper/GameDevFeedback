4fcfbc7e6d7631d0c886d8ef4d311c1ba506ed85
Elbek <52498074+ElbekDeveloper@users.noreply.github.com>
2020-12-22 12:19:02 +0500

    refactor: delete unimportant code

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -3,7 +3,6 @@
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _myBody;
-    private float _moveSpeed = 2f;
 
     void Awake()
     {
@@ -13,31 +12,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //float horizontalMovement = GetHorizontalInput();
-        //Move(horizontalMovement);
+
     }
-
-    private float GetHorizontalInput()
-    {
-        return Input.GetAxisRaw("Horizontal");
-    }
-
-    void Move(float horizontalPosition)
-    {
-        if (horizontalPosition > 0f)
-        {
-            _myBody.velocity = new Vector2(_moveSpeed, _myBody.velocity.y);
-        }
-
-        if(horizontalPosition < 0f)
-        {
-            _myBody.velocity = new Vector2(-_moveSpeed, _myBody.velocity.y);
-        }
-
-    } //Move
-
-
-
 
     public void PlatformMove(float x)
     {
